5b85b0d9de5684beb02e969a6f6091412c586b29
Adrian Sanchez <aamiel.sanchez@gmail.com>
2024-02-19 00:52:58 +0100

    fix(file-tree): fix file tree toggling

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -1,15 +1,25 @@
+
 return {
     {
         'nvim-tree/nvim-tree.lua',
         tag = 'v0.99',
+        config = true,
         init = function()
+            local focusOrToggle = function ()
+                local nvim_tree = require("nvim-tree.api")
+                local current_buf = vim.api.nvim_get_current_buf()
+                local current_buftype = vim.api.nvim_get_option_value("filetype", { buf = current_buf })
+                if current_buftype == "NvimTree" then
+                    nvim_tree.tree.toggle()
+                else
+                    nvim_tree.tree.focus()
+                end
+            end
+
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
+            vim.keymap.set("n", "<C-t>", focusOrToggle)
         end,
-        config = true,
-        keys = {
-            { '<C-t>', vim.cmd.NvimTreeToggle }
-        }
     },
     {
         'nvim-tree/nvim-web-devicons',
