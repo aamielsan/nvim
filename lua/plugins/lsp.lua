f5924029b563f7bab04ff6689f5cb3224d7ba333
Adrian Sanchez <aamiel.sanchez@gmail.com>
2024-02-18 21:49:21 +0100

    fix(lsp): fix gopls configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,13 +16,6 @@ return {
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v3.x',
-        lazy = true,
-        config = false,
-        init = function()
-            -- Disable automatic setup, we are doing it manually
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-        end,
     },
     {
         'williamboman/mason.nvim',
@@ -77,18 +70,22 @@ return {
             lsp_zero.on_attach(function(client, bufnr)
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
-                lsp_zero.default_keymaps({buffer = bufnr})
+                lsp_zero.default_keymaps({ buffer = bufnr })
             end)
 
             require('mason-lspconfig').setup({
                 ensure_installed = lsp_servers,
                 handlers = {
                     lsp_zero.default_setup,
-                    lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require('lspconfig').lua_ls.setup(lua_opts)
-                    end,
+                    gopls = function()
+                        require('lspconfig').gopls.setup({
+                            settings = {
+                                gopls = {
+                                    usePlaceholders = true
+                                }
+                            }
+                        })
+                    end
                 }
             })
         end
