c42540ec10c76e194a898fb7fb225efc3c10f415
Adrian Sanchez <aamiel.sanchez@gmail.com>
2024-02-18 20:47:08 +0100

    fix: migrate old settings to lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,12 @@
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<C-C>:update<CR>")
+local keymap = vim.keymap
+local silent = { silent = true }
+
+keymap.set("n", "Y", "y$")
+keymap.set("i", "jj", "<Esc>")
+keymap.set("i", "jk", "<Esc>")
+
+keymap.set("n", "j", "gj")
+keymap.set("n", "k", "gk")
+
+keymap.set("n", "<Esc>", ":nohl<CR><Esc>", silent)
+keymap.set({ "n", "i", "v" }, "<C-s>", "<C-C>:update<CR>", silent)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,41 @@ vim.g.mapleader = " "
 local opt = vim.opt
 
 opt.clipboard = "unnamedplus" -- use system clipboard
+
+-- ui
 opt.termguicolors = true -- true color support
 opt.cursorline = true -- highlight current line
+opt.wrap = false -- nowrap
 
+-- line numbers
 opt.number = true -- show line numbers
 opt.relativenumber = true -- show relative line numbers
+
+-- indents
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.smartindent = true
+opt.autoindent = true
+
+opt.shiftround = true -- round-off indents to align
+-- opt.foldmethod = "indent"
+
+-- backups
+opt.swapfile = false
+opt.backup = false
+
+-- undo history
+opt.undofile = true
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- search
+opt.hlsearch = true
+opt.incsearch = true
+
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+opt.updatetime = 50
