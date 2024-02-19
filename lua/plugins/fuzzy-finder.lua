2690a2eeec9ff342cc9c34e42eca345ac83fee33
Adrian Sanchez <aamiel.sanchez@gmail.com>
2024-02-19 21:59:13 +0100

    feat(fuzzy): update keymapping for buffer navigation

--- lua/plugins/fuzzy-finder.lua
+++ lua/plugins/fuzzy-finder.lua
@@ -5,10 +5,25 @@ return {
         tag = '0.1.5',
         -- also depends on ripgrep: `brew install ripgrep`
         dependencies = { 'nvim-lua/plenary.nvim' },
-        config = true,
+        config = function()
+            require("telescope").setup({
+                pickers = {
+                    buffers = {
+                        ignore_current_buffer = true,
+                        sort_lastused = true,
+                        mappings = {
+                            i = {
+                                ["<C-q>"] = "delete_buffer",
+                            }
+                        }
+                    },
+                }
+            })
+        end,
         keys = {
             { '<Leader>f', function() require('telescope.builtin').live_grep({}) end },
             { '<Leader>p', function() require('telescope.builtin').find_files() end },
+            { '<Leader>o', function() require('telescope.builtin').buffers() end },
         }
     }
 }
