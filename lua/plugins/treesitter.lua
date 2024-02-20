bfe87bf95966a1b56099445f435f48daee531f0f
Adrian Sanchez <aamiel.sanchez@gmail.com>
2024-02-20 09:27:25 +0100

    fix(treesitter): setup not working

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,48 +4,52 @@ return {
         "nvim-treesitter/nvim-treesitter",
         version = "v0.9.2",
         build = ":TSUpdate",
-        setup = {
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            -- Reference: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-            ensure_installed = {
-                -- required
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                -- Reference: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
+                ensure_installed = {
+                    -- required
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
 
-                -- optional
-                "go",
-                "javascript",
-                "json",
-                "kotlin",
-                "lua",
-                "typescript",
-                "yaml",
-            },
+                    -- optional
+                    "go",
+                    "gomod",
+                    "gosum",
+                    "gowork",
+                    "javascript",
+                    "json",
+                    "kotlin",
+                    "typescript",
+                    "yaml",
+                },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+                auto_install = false,
 
-            highlight = {
-                enable = true,
+                highlight = {
+                    enable = true,
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        }
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
     },
     -- https://github.com/windwp/nvim-autopairs
     {
-        'windwp/nvim-autopairs',
+        "windwp/nvim-autopairs",
         event = "InsertEnter",
         config = true
     },
