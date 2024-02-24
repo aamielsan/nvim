8dca8cbe5ade582c76abc12f0edc97b5366e8e95
Adrian Sanchez <aamiel.sanchez@gmail.com>
2024-02-24 20:42:10 +0100

    chore(fuzzy-finder: show current buffer in telescope

--- lua/plugins/fuzzy-finder.lua
+++ lua/plugins/fuzzy-finder.lua
@@ -9,7 +9,6 @@ return {
             require("telescope").setup({
                 pickers = {
                     buffers = {
-                        ignore_current_buffer = true,
                         sort_lastused = true,
                         mappings = {
                             n = {
